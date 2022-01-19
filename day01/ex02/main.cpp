8ca95d17828884faec0568a222e4d4f621fb8713
simon ameye <speedhacker@MacBook-Air-de-simon.local>
2022-01-19 16:39:42 +0100

    add

---
+++ day01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sameye <sameye@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 16:12:07 by sameye            #+#    #+#             */
+/*   Updated: 2022/01/19 16:39:10 by sameye           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << &string << std::endl; //Address of the string using the address of the string
+	std::cout << stringPTR << std::endl; //Address of the string using the pointer
+	std::cout << &stringREF << std::endl; //Address of the string using the reference
+
+	std::cout << *stringPTR << std::endl; //String using the pointer
+	std::cout << stringREF << std::endl; //String using the ref
+}
