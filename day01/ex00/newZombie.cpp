7caaf60eda4fc66c560efdcfe2b3c0d0410f4ec8
simon ameye <speedhacker@MacBook-Air-de-simon.local>
2022-01-20 13:50:17 +0100

    yo

--- day01/ex00/newZombie.cpp
+++ day01/ex00/newZombie.cpp
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sameye <sameye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/17 16:41:12 by sameye            #+#    #+#             */
-/*   Updated: 2022/01/19 11:53:42 by sameye           ###   ########.fr       */
+/*   Created: 2022/01/19 11:47:02 by sameye            #+#    #+#             */
+/*   Updated: 2022/01/19 11:54:02 by sameye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <iostream>
 
-Zombie::Zombie(void)
+Zombie*	newZombie(std::string name)
 {
-	return ;
-}
-
-Zombie::~Zombie(void)
-{
-	std::cout << this->_name << " has been destroyed" << std::endl;
-}
-
-void Zombie::announce(void)
-{
-	std::cout << _name << " BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
-void Zombie::give_name(std::string name)
-{
-		this->_name = name;
+	Zombie*	newZombie	= new Zombie();
+	newZombie->give_name(name);
+	return (newZombie);
 }
--- day01/ex00/randomChump.cpp
+++ day01/ex00/randomChump.cpp
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sameye <sameye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/17 16:41:12 by sameye            #+#    #+#             */
-/*   Updated: 2022/01/19 11:53:42 by sameye           ###   ########.fr       */
+/*   Created: 2022/01/19 11:47:05 by sameye            #+#    #+#             */
+/*   Updated: 2022/01/19 11:53:26 by sameye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <iostream>
 
-Zombie::Zombie(void)
+void	randomChump(std::string name)
 {
-	return ;
-}
-
-Zombie::~Zombie(void)
-{
-	std::cout << this->_name << " has been destroyed" << std::endl;
-}
-
-void Zombie::announce(void)
-{
-	std::cout << _name << " BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
-void Zombie::give_name(std::string name)
-{
-		this->_name = name;
+	Zombie *zombie = newZombie(name);
+	zombie->announce();
+	delete zombie;
 }
